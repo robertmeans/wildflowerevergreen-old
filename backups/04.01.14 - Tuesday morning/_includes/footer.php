6f9e1c7cd566091dab79f4c1d4e24358899dd0b0
robertmeans <robert@robertmeans.com>
2015-04-01 11:46:49 -0600

    update and delete
    updated .gitignore and deleted backups dir

--- backups/04.01.14 - Tuesday morning/_includes/footer.php
+++
@@ -1,4 +0,0 @@
-<footer>
-	&copy; <a class="design" href="http://www.robertmeans.com" target="_blank">design</a> 2014 Wildflower Caf&eacute;, 28035 Highway 74, Evergreen, Colorado 80439 - 303.674.3323
-
-</footer>
--- backups/04.30.14 - before messing with nav/_includes/footer.php
+++
@@ -1,4 +0,0 @@
-<footer>
-	&copy; 2014 Wildflower Caf&eacute;, 28035 Highway 74, Evergreen, Colorado 80439 - 303.674.3323
-
-</footer>
